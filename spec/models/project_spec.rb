75d5c0c3bf1280d724f9049f7d201821003c394d
Alex Reyes <alejandroereyes@gmail.com>
2015-10-25 11:01:14 -0500

    updated specs to use implicit matcher for predicate method done?

--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -6,11 +6,11 @@ RSpec.describe Project do
   let(:task)    { Task.new }
 
   it 'considers a project with no tasks to be done' do
-    expect(project.done?).to be_truthy
+    expect(project).to be_done # using the implicit matcher 'done' from be_done will be set as predicate method on project ie. project.done?
   end
 
   it 'knows that a project with an imcomplete task is not done' do
     project.tasks << task
-    expect(project.done?).to be_falsy
+    expect(project).to_not be_done #using the implicit matcher do use done as predicate method for project ie. project.done? will be called
   end
 end
