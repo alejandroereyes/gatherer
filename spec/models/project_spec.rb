0c1bbe8120fea080dce1169f89ad83cbe244822e
Alex Reyes <alejandroereyes@gmail.com>
2015-12-12 16:38:34 -0600

    updated project spec to test estimates on blank project and prod code

--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -2,22 +2,31 @@ require 'rails_helper'
 
 RSpec.describe Project do
 
-  let(:project) { Project.new }
-  let(:task)    { Task.new }
+  describe "initialization" do
+    let(:project) { Project.new }
+    let(:task)    { Task.new }
 
-  it 'considers a project with no tasks to be done' do
-    expect(project).to be_done # using the implicit matcher 'done' from be_done will be set as predicate method on project ie. project.done?
-  end
+    it 'considers a project with no tasks to be done' do
+      expect(project).to be_done # using the implicit matcher 'done' from be_done will be set as predicate method on project ie. project.done?
+    end
 
-  it 'knows that a project with an imcomplete task is not done' do
-    project.tasks << task
-    expect(project).to_not be_done #using the implicit matcher do use done as predicate method for project ie. project.done? will be called
-  end
+    it 'knows that a project with an imcomplete task is not done' do
+      project.tasks << task
+      expect(project).to_not be_done #using the implicit matcher do use done as predicate method for project ie. project.done? will be called
+    end
 
-  it 'marks a project done if its task are done' do
-    project.tasks << task
-    task.mark_completed
-    expect(project).to be_done
+    it 'marks a project done if its task are done' do
+      project.tasks << task
+      task.mark_completed
+      expect(project).to be_done
+    end
+
+    it "properly estimates a blank project" do
+      expect(project.completed_velocity).to eq(0)
+      expect(project.current_rate).to eq(0)
+      expect(project.projected_days_remaining.nan?).to be_truthy
+      expect(project).not_to be_on_schedule
+    end
   end
 
   describe 'estimates' do
