cf164ad5575e062919a1638b7660875d2953c515
Alex Reyes <alejandroereyes@gmail.com>
2015-12-21 18:19:54 -0600

    added spec to show composite spec

--- spec/actions/creates_project_spec.rb
+++ spec/actions/creates_project_spec.rb
@@ -35,6 +35,12 @@ describe CreatesProject do
       specify { expect(tasks.size).to eq(2) }
       specify { expect(tasks.map(&:title)).to eq(["Start things", "End things"]) }
       specify { expect(tasks.map(&:size)).to eq([3,2]) }
+
+      it "is another way to right this spec using a composite spec" do
+        expect(tasks).to match([
+          an_object_having_attributes(title: "Start things", size: 3),
+          an_object_having_attributes(title: "End things", size: 2)])
+      end
     end
 
     describe "attaches tasks to the project" do
