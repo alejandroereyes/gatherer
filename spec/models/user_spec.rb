0aff85d61c482d423a5e1aac1785bc8b9049f18d
Alex Reyes <alejandroereyes@gmail.com>
2015-12-29 12:13:39 -0600

    added spec for happy user can view path and refactored created objects

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,15 +2,19 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
 
+  let(:user)    { FactoryGirl.create(:user) }
+  let(:project) { FactoryGirl.create(:project) }
+
   it "cannot view a project it is not a part of" do
-    user = User.new
-    project = Project.new
     expect(user.can_view?(project)).to be_falsy
   end
 
+  it "can view a project it is a part of" do
+    user.roles.create!(project: project)
+    expect(user.can_view?(project)).to be_truthy
+    end
+
   describe "public roles" do
-    let(:user) { User.new }
-    let(:project) { Project.new }
 
     it "allows an admin to view a project" do
       user.admin = true
