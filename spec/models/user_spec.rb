8f3e7304118fa0dceea9dad3b46470d60342003b
Alex Reyes <alejandroereyes@gmail.com>
2015-12-28 21:02:24 -0600

    added spec for user can view? a project and prod

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  # pending "add some examples to (or delete) #{__FILE__}"
+
+  it "cannot view a project it is not a part of" do
+    user = User.new
+    project = Project.new
+    expect(user.can_view?(project)).to be_falsy
+  end
 end
